a9bcdd38676c5539d97ec6764e91625a3c8b3f37
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2024-04-15 17:35:06 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '/node_modules/bootstrap/dist/js/bootstrap.min.js';
 import './App.css';
 import Home from './Home';
+import LanguageView from './components/language/LanguageView';
 
 function App() {
   return (
     <div className="App">
      <h2>Welcome to the front-end</h2>
      <Home />
+     <LanguageView />
     </div>
   );
 }
--- src/components/language/LanguageView.js
+++ src/components/language/LanguageView.js
@@ -1,22 +1,63 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
 
 const LanguageView = () => {
-  return (
-    <section>
-      <table>
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Language</th>
-                <th>Country</th>
-                <th>Country Flag</th>
-            </tr>
-        </thead>
-        <tbody></tbody>
-      </table>
-    </section>
-  )
+    const [languages, setLanguages] = useState([]);
+
+    useEffect(() => {
+        loadLanguages();
+    }, []);
+
+    const loadLanguages = async () => {
+        const result = await axios.get(
+            "http://localhost:9192/languages",
+            {
+                validateStatus: () => {
+                    return true;
+                },
+            }
+        );
+        if (result.status === 302) {
+            setLanguages(result.data);
+        }
+    }
+
+    return (
+        <section>
+            <table className='table table-bordered table-hover'>
+                <thead>
+                    <tr className='text-center'>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Language</th>
+                        <th>Country</th>
+                        <th colSpan='3'>Actions</th>
+                    </tr>
+                </thead>
+                <tbody className='text-center'>
+                    {languages.map((language, index) => (
+                        <tr key={language.id}>
+                            <th scope='row' key={index}>
+                                {index + 1}
+                            </th>
+                            <td>{language.fullName}</td>
+                            <td>{language.language}</td>
+                            <td>{language.country}</td>
+                            <td className='mx-2'>
+                                <button className='btn btn-info'>View</button>
+                            </td>
+                            <td className='mx-2'>
+                            <button className='btn btn-warning'>Edit</button>
+                            </td>
+                            <td className='mx-2'>
+                            <button className='btn btn-danger'>Delete</button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </section>
+    )
 }
 
 export default LanguageView
