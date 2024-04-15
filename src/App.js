1dda395617c83c2b4ab9e5d183cdb415bb6fe349
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2024-04-15 16:31:05 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
      <h2>Welcome to the front-end</h2>
+     <Home />
     </div>
   );
 }
