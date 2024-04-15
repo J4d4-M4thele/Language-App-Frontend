4b119abb20bc44174e38a897552784b2b4b597c4
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2024-04-15 16:31:49 +0200

    first commit

---
+++ src/components/language/LanguageView.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const LanguageView = () => {
+  return (
+    <section>
+      <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Language</th>
+                <th>Country</th>
+                <th>Country Flag</th>
+            </tr>
+        </thead>
+        <tbody></tbody>
+      </table>
+    </section>
+  )
+}
+
+export default LanguageView
