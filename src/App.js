6b8898dc8f1bd036767128348c586de0a557c21d
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2024-04-15 17:51:44 +0200

    update

--- src/App.js
+++ src/App.js
@@ -3,13 +3,27 @@ import '/node_modules/bootstrap/dist/js/bootstrap.min.js';
 import './App.css';
 import Home from './Home';
 import LanguageView from './components/language/LanguageView';
+import NavBar from './components/common/NavBar';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-     <h2>Welcome to the front-end</h2>
-     <Home />
-     <LanguageView />
+     <Router>
+     <NavBar />
+      <Routes>
+        <Route
+        exact 
+        path='/'
+        element={<Home />}
+        ></Route>
+        <Route
+        exact 
+        path='/view-students'
+        element={<LanguageView />}
+        ></Route>
+      </Routes>
+     </Router>
     </div>
   );
 }
