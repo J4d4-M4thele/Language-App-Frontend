330e224a33f4b04bf7e4dffb811bfbc21a105ba6
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2024-04-16 11:48:14 +0200

    update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './Home';
 import LanguageView from './components/language/LanguageView';
 import NavBar from './components/common/NavBar';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import AddLanguage from './components/language/AddLanguage';
 
 function App() {
   return (
@@ -21,6 +22,11 @@ function App() {
         exact 
         path='/view-languages'
         element={<LanguageView />}
+        ></Route>
+        <Route
+        exact 
+        path='/add-languages'
+        element={<AddLanguage />}
         ></Route>
       </Routes>
      </Router>
